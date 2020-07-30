63b66c110dd763bc4ec956db65a4f5a0a8355eb0
Rubem <rfsv@ic.ufal.br>
2020-07-29 21:49:20 -0300

    Add router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+	return (
+		<div>
+			Página de Cadastro de Vídeo
+		</div>
+	);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" component={App} exact />
+			<Route path="/cadastro/video" component={CadastroVideo} />
+			<Route component={() => (<div>Página 404</div>)} />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('root')
 );
